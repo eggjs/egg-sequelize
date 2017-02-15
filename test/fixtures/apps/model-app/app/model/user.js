736d247434269c18976a2d2c09a81392232a4866
Hongcai Deng <admin@dhchouse.com>
2017-02-15 17:08:49 +0800

    chore: complete unittest (#2)

--- test/fixtures/apps/model-app/app/model/user.js
+++ test/fixtures/apps/model-app/app/model/user.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const assert = require('assert');
+
 module.exports = sequelize => {
   return sequelize.define('user', {
     name: {
       type: sequelize.Sequelize.STRING(30),
+    },
+  }, {
+    classMethods: {
+      associate(models) {
+        assert.ok(models.user);
+      },
     },
   });
 };
