8b70775a1203fcf6d6090a41e9452b24d71946a1
microud <201458501212@ytu.edu.cn>
2019-01-07 09:20:03 +0800

    feat: support custom egg-sequelize config

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
 import * as sequelize from "sequelize";
 
+interface EggSequelizeOptions extends sequelize.Options {
+  delegate?: string;
+  baseDir?: string;
+}
+
 interface DataSources {
-  [datasources]: sequelize.Options;
+  [datasources]: EggSequelizeOptions;
 }
 
 declare module 'egg' {
@@ -19,7 +24,7 @@ declare module 'egg' {
 
   // extend your config
   interface EggAppConfig {
-    sequelize: sequelize.Options | DataSources;
+    sequelize: EggSequelizeOptions | DataSources;
   }
 
 }
