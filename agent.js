4787676155cf4e815088303ba471f60cbe933e74
liuhang <1016817543@qq.com>
2019-07-09 16:15:38 +0800

    修改启动文件app.js、agent.js，将app.beforeStart替换为新版的启动自定义

--- agent.js
+++ agent.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = agent => {
-  require('./lib/loader')(agent);
-};
+class AgentBootHook {
+
+  constructor(agent) {
+    this.agent = agent;
+  }
+
+  async didLoad() {
+    // 请将你的插件项目中 app.beforeStart 中的代码置于此处。
+    await require('./lib/loader')(this.agent);
+  }
+
+}
+
+module.exports = AgentBootHook;
--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-module.exports = app => {
-  require('./lib/loader')(app);
-};
+class AppBootHook {
 
+  constructor(app) {
+    this.app = app;
+  }
+
+  async didLoad() {
+    // 请将你的插件项目中 app.beforeStart 中的代码置于此处。
+    await require('./lib/loader')(this.app);
+  }
+
+}
+
+module.exports = AppBootHook;
